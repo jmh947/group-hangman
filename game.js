380d6935b252d2a4acd4608e5847a0ca7bb76985
Dylan Ambrose <djambro44@gmail.com>
2020-02-22 12:47:39 -0700

    psuedo code

---
+++ game.js
@@ -0,0 +1,39 @@
+//set variables 
+//      userGuess
+//      words array 
+//      selectedWord
+//      splitSelectedWord 
+//      underscores
+//      remainingGuesses
+//      lettersGuessed
+//      wins
+//      losses
+
+// set functions
+
+//      1)function start game
+//          a)set remaining guesses to 15
+//          b)set letters guessed to empty (maybe make lettersGuessed an array)
+//          c) run function getRandomWord
+
+//      2) function getRandomWord
+//          a)pick random word from words array and set it to selectedWord
+//          b)split selected word so we can get individual characters
+
+//      3) function getUnderscores
+//          a)run a for loop to set an "_" for each character in splitSelectedWord
+
+//      4)function getUserInput
+//          a)get user click and save it as UserInput
+
+//      5)function wrong
+//          a)user losses one guess (remainingGuesses--)
+//          b)userGuess gets saved to lettersGuessed array and is shown in letters guessed box(html)
+
+//      6)function right
+//          a)userInput replaces underscore in shown word (html)
+
+//      7)function gameOver
+//          a)prompt "GAME OVER! press Button to start a new game!"
+
+
